116d3c4b60afd743a059d6b49ee104bdcb922597
Shrikant Patnaik <shrikant.patnaik@generalsensing.com>
2014-03-08 01:14:34 +0800

    Another try at disabling registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :confirmable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Flag used to identify if the user was found or created from find_or_create
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Fulcrum::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Disable Registrations
+  config.fulcrum.disable_registration = true
 end
