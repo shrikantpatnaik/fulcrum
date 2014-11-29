002cd08215ba71264d42c0d7f1e3903509399be4
shrikant patnaik <shrikant.patnaik@gmail.com>
2014-11-29 21:24:34 +0800

    add precompile to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Fulcrum::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.initialize_on_precompile = true
 end
